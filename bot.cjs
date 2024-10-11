95360b896bf53c83399bf2916e2f7e2d0c545277
cadillac-cyrus <myredevesuper@gmail.com>
2024-10-11 17:49:54 +0000

    port again

--- bot.cjs
+++ bot.cjs
@@ -212,6 +212,10 @@ bot.on('message', async (msg) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
